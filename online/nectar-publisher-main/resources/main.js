887befe1a1c006bdc33d89299dd4cb4cc0399d32
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2025-12-08 17:04:23 +0100

    Update

--- online/nectar-publisher-main/resources/main.js
+++ online/nectar-publisher-main/resources/main.js
@@ -506,7 +506,7 @@ const app = createApp({
       let url;
       if (params.fileid && params.siteUrl && params.datasetid && params.datasetversion && params.locale) {
         root = 'https://cdif-4-xas.dev.codata.org';
-        siteUrl = 'http://dataverse:8080';
+        //siteUrl = 'http://dataverse:8080';
         url = root + '/cdi?fileid=' + encodeURIComponent(params.fileid)
           + '&siteUrl=' + encodeURIComponent(siteUrl)
           + '&datasetid=' + encodeURIComponent(params.datasetid)
